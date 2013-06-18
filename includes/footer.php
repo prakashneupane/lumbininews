4eac8b0c699e61b348a1012b30a79898a2811bfb
prakashneupane <nprkshn@gmail.com>
2013-06-18 12:53:45 +0545

    Added Fresh new design

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<div class="wrapper col8">
+  <div id="copyright">
+    <p class="fl_left">Copyright &copy; All Rights Reserved<a href="#"> www.newspost.com</a></p>
+    <p class="fl_right">Powered by :<a href="http://www.shatkonmedia.com/" target="_blank"> Shatkon Media</a></p>
+    <br class="clear" />
+  </div>
+</div>
